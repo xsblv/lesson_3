dcaf0dd5371f4b41809bd5266d20112edd6e817d
Ksenya Soboleva <w673rx89@yandex.ru>
2025-09-10 19:45:28 +0500

    Изменено разрешение браузера на 1200х800

--- conftest.py
+++ conftest.py
@@ -4,15 +4,15 @@ import pytest
 
 @pytest.fixture()
 def browser_size():
-    browser.config.window_width = 360
-    browser.config.window_height = 712
+    browser.config.window_width = 1200
+    browser.config.window_height = 800
 
 
 @pytest.fixture()
 def open_browser(browser_size):
     browser.open('https://www.drive2.ru/')
-    assert browser.config.window_width==360,"Ширина окна не соответствует заданному значению"
-    assert browser.config.window_height==712, "Высота окна не соответствует заданному значению"
+    assert browser.config.window_width == 1200, "Ширина окна не соответствует заданному значению"
+    assert browser.config.window_height == 800, "Высота окна не соответствует заданному значению"
     yield browser
     browser.quit()
 
